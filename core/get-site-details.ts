63eafa8f0bb3ac985054d8aecd37e24ebb42db02
Kathiravan Balu <kathir@arizon.digital>
2025-01-09 20:10:33 +0530

    domain update

--- core/get-site-details.ts
+++ core/get-site-details.ts
@@ -1,11 +1,13 @@
 import { unstable_noStore as noStore } from 'next/cache';
+import { cookies } from 'next/headers';
 import { kv } from "./lib/kv";
 import { SiteDataCache } from "./middlewares/with-routes";
 
 export async function getChannelIdFromSite() {
   noStore();
-  let channelData = await kv.get<SiteDataCache>('Belami_localhost:3000_v3_sitedata');
-  console.log('========channelData=======', channelData);
+  let cookieStore = await cookies();
+  let domain = cookieStore.get('domain');
+  let channelData = await kv.get<SiteDataCache>(`${process.env.KV_NAMESPACE}_${domain}_v3_sitedata`);
   if(channelData?.siteData?.length) {
     let siteData: any = channelData?.siteData?.[0];
     if(siteData?.channelId) {
@@ -17,7 +19,9 @@ export async function getChannelIdFromSite() {
 
 export async function getStoreFrontTokenFromSite() {
   noStore();
-  let channelData = await kv.get<SiteDataCache>('Belami_localhost:3000_v3_sitedata');
+  let cookieStore = await cookies();
+  let domain = cookieStore.get('domain');
+  let channelData = await kv.get<SiteDataCache>(`${process.env.KV_NAMESPACE}_${domain}_v3_sitedata`);
   console.log('========storee=======', channelData);
   if(channelData?.siteData?.length) {
     let siteData: any = channelData?.siteData?.[0];
