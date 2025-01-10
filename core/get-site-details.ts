862014c20f283f088e4dc092213640d558abf58d
Kathiravan Balu <kathir@arizon.digital>
2025-01-10 20:12:46 +0530

    cookie fix

--- core/get-site-details.ts
+++ core/get-site-details.ts
@@ -1,12 +1,12 @@
 import { unstable_noStore as noStore } from 'next/cache';
-import { cookies } from 'next/headers';
 import { kv } from "./lib/kv";
 import { SiteDataCache } from "./middlewares/with-routes";
 
 export async function getChannelIdFromSite() {
   noStore();
-  let cookieStore = await cookies();
-  let domain = cookieStore.get('domain');
+  //let cookieStore = await cookies();
+  let domain = "";
+  console.log('========domain=======', domain);
   let channelData = await kv.get<SiteDataCache>(`${process.env.KV_NAMESPACE}_${domain}_v3_sitedata`);
   if(channelData?.siteData?.length) {
     let siteData: any = channelData?.siteData?.[0];
@@ -19,8 +19,9 @@ export async function getChannelIdFromSite() {
 
 export async function getStoreFrontTokenFromSite() {
   noStore();
-  let cookieStore = await cookies();
-  let domain = cookieStore.get('domain');
+  //let cookieStore = await cookies();
+  let domain = "";
+  console.log('========domain=======', domain);
   let channelData = await kv.get<SiteDataCache>(`${process.env.KV_NAMESPACE}_${domain}_v3_sitedata`);
   console.log('========storee=======', channelData);
   if(channelData?.siteData?.length) {
